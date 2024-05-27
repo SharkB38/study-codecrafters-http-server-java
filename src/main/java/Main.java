5d348f52e8a5ecf4c286ea067ab25f944229cb4f
SharkB <val.wolf.ne@gmail.com>
2024-05-27 20:47:35 +0900

    pass stage 5 test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,25 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
-  public static void main(String[] args) {
+
+    public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     //
     //
-     try {
+     try (final ExecutorService executorService = Executors.newFixedThreadPool(10)) {
          ServerSocket serverSocket = new ServerSocket(4221);
          serverSocket.setReuseAddress(true);
-         Socket clientSocket = serverSocket.accept();
-         System.out.println("new connection accepted");
-         (new ResponseHandler(clientSocket)).start();
        while (true) {
-           clientSocket = serverSocket.accept();
+           Socket clientSocket = serverSocket.accept();
            System.out.println("new connection accepted");
-           (new ResponseHandler(clientSocket)).start();
+           executorService.submit(new ResponseHandler(clientSocket));
        }
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
